d5c72ab5cd829fd2001f76bffbfa03d774f37466
Keith Jung <60eokk@gmail.com>
2023-12-21 18:25:08 +0900

    Initial Commit

---
+++ HOOWHO/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HOOWHO
+//
+//  Created by Keith Jung on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HOOWHO/HOOWHOApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOOWHOApp.swift
+//  HOOWHO
+//
+//  Created by Keith Jung on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HOOWHOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
