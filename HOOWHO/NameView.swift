8d70d056c1a0ed640f5c38670888ec8dde23d8b8
Keith Jung <60eokk@gmail.com>
2023-12-23 17:15:48 +0900

    Added NameView, Pollpage, ProfilePic, Username swift files

---
+++ HOOWHO/NameView.swift
@@ -0,0 +1,32 @@
+//
+//  NameView.swift
+//  HOOWHO
+//
+//  Created by Keith Jung on 12/21/23.
+//
+
+
+import SwiftUI
+
+struct NameView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("nextpage")
+                    .font(.largeTitle)
+                    .padding()
+
+            }
+        }
+    }
+}
+
+
+
+
+
+struct NameView_Previews: PreviewProvider {
+    static var previews: some View {
+        NameView()
+    }
+}
---
+++ HOOWHO/ProfilePic.swift
@@ -0,0 +1,33 @@
+//
+//  ProfilePic.swift
+//  HOOWHO
+//
+//  Created by Keith Jung on 12/21/23.
+//
+
+
+
+import SwiftUI
+
+struct ProfilePic: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("nextpage")
+                    .font(.largeTitle)
+                    .padding()
+
+            }
+        }
+    }
+}
+
+
+
+
+
+struct ProfilePic_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePic()
+    }
+}
