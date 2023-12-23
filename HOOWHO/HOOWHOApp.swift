36c78580ad92e254ad65efa2e077dbf3b181443d
Keith Jung <60eokk@gmail.com>
2023-12-23 18:26:52 +0900

    Made project in Firebase with bundle name KEITH.HOOWHO and have added packages accordingly

--- HOOWHO/HOOWHOApp.swift
+++ HOOWHO/HOOWHOApp.swift
@@ -5,16 +5,43 @@
 //  Created by Keith Jung on 12/21/23.
 //
 
+//import SwiftUI
+//
+//@main
+//struct HOOWHOApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            LaunchView()
+//        }
+//    }
+//}
+
+
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct HOOWHOApp: App {
-    var body: some Scene {
-        WindowGroup {
-            LaunchView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        LaunchView()
+      }
     }
+  }
 }
-
-
-
