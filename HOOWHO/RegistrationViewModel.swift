17c0cf75a6930be49f019968da67da67c00e2eec
Keith Jung <60eokk@gmail.com>
2023-12-25 18:33:19 +0900

    removed decoding json, and instead directly used firebase authentication, so when the user types in registration information, it will show up in the firebase database, and it will also check whether info has been already used or not

--- HOOWHO/RegistrationViewModel.swift
+++ HOOWHO/RegistrationViewModel.swift
@@ -7,67 +7,29 @@
 
 
 import Foundation
+import FirebaseAuth // Import FirebaseAuth
 
 class RegistrationViewModel: ObservableObject {
     @Published var isRegistrationSuccessful = false
-    
-    struct RegistrationResponse: Decodable {
-        let success: Bool
-        // Include other fields based on your API's response
-    }
-
+    @Published var errorMessage: String? // Optional: To hold any error message
 
     func registerWithEmail(_ email: String, password: String) {
-        // The URL for the registration endpoint
-        guard let url = URL(string: "https://yourserver.com/register") else {
-            print("Invalid URL")
-            return
-        }
-
-        // Create a URLRequest object
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
-        // Prepare the data to be sent in the request
-        let body: [String: Any] = ["email": email, "password": password]
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: body, options: []) else {
-            print("Error: Cannot create JSON from user data")
-            return
-        }
-
-        // Set the request body
-        request.httpBody = jsonData
-
-        // Perform the network request
-        URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
-            // Ensure there's no error and data was received
-            guard let data = data, error == nil else {
-                print("Network request error: \(error?.localizedDescription ?? "Unknown error")")
-                return
-            }
-
-            // Process the response
-            do {
-                // Decode the response (adjust according to your API's response structure)
-                let response = try JSONDecoder().decode(RegistrationResponse.self, from: data)
-                
-                // Update the state based on the response
-                DispatchQueue.main.async {
-                    self?.isRegistrationSuccessful = response.success
-                    // Handle more specific states if necessary, like showing error messages
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self] authResult, error in
+            DispatchQueue.main.async {
+                if let user = authResult?.user {
+                    print("User registered: \(user)")
+                    self?.isRegistrationSuccessful = true
+                } else if let error = error {
+                    print("Registration failed: \(error.localizedDescription)")
+                    self?.errorMessage = error.localizedDescription
                 }
-            } catch {
-                print("Failed to decode JSON: \(error)")
             }
-        }.resume()
+        }
     }
-
-    
-    
 
     func signInWithGoogle() {
         // Implement Google sign-in logic here
+        // This typically involves integrating a Google Sign-In SDK and then
+        // authenticating with Firebase using the credentials obtained from Google.
     }
 }
-
