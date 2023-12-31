14f471155dc16aac673083ab49de732feb0f36f7
Keith Jung <60eokk@gmail.com>
2023-12-31 19:19:22 +0900

    Added comments for LaunchView

--- HOOWHO/HOOWHOApp.swift
+++ HOOWHO/HOOWHOApp.swift
@@ -1,4 +1,4 @@
-//
+
 //  HOOWHOApp.swift
 
 
--- HOOWHO/LaunchView.swift
+++ HOOWHO/LaunchView.swift
@@ -1,23 +1,23 @@
 //
 //  LaunchView.swift
-//  HOOWHO
-//
-//  Created by Keith Jung on 12/21/23.
-//
+
+// Differences between State, StateObject, ObservedObject, EnvironmentObject: https://www.hackingwithswift.com/quick-start/swiftui/whats-the-difference-between-observedobject-state-and-environmentobject
+
 
 import SwiftUI
 
-struct LaunchView: View {
-    @EnvironmentObject var appState: AppState
-    var body: some View {
-        NavigationView {
-            VStack {
+//In SwiftUI, every piece of UI is "View"
+struct LaunchView: View { //Defines new view type called LaunchView, which conforms to View protocol
+    @EnvironmentObject var appState: AppState //Declares environment object named appState
+    var body: some View {//Obligates LaunchView to to have property of "body". body is a mandatory property for any type that conforms with View protocol
+        NavigationView { //Container view that manages hierarchical content, typically used to create and manage navigation stack
+            VStack { //Vertical stack that arranges its child views in vertical order
                 Text("HOOWHO")
                     .font(.largeTitle)
                     .padding()
 
-                NavigationLink(destination: GradePickerView()) {
-                    Text("👤")
+                NavigationLink(destination: GradePickerView()) { //Navigation link, when tapped, transitions to GradePickerView
+                    Text("👤") //Content of NavigationLink
                         .font(.system(size: 50))
                 }
             }
@@ -28,8 +28,7 @@ struct LaunchView: View {
 
 
 
-
-struct LaunchView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider { //Used by Xcode to render a preview
     static var previews: some View {
         LaunchView()
     }
