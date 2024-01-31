2f48513d89f08b368dd99c18cecc5167924f3e91
Keith Jung <60eokk@gmail.com>
2024-01-31 12:49:34 -0600

    not navigating after register button

--- HOOWHO/aRegisterView.swift
+++ HOOWHO/aRegisterView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct aRegisterView: View {
     @State private var email = ""
     @State private var password = ""
+    @State private var isRegistered = false // State to control navigation
     
     var body: some View {
         Form {
@@ -10,8 +11,32 @@ struct aRegisterView: View {
             SecureField("Password", text: $password)
             Button("Register") {
                 // Implement registration logic here
+                self.isRegistered = true
             }
         }
         .navigationTitle("Register")
     }
 }
+
+
+//
+//
+//    var body: some View {
+//        Form {
+//            TextField("Email", text: $email)
+//            SecureField("Password", text: $password)
+//            Button("Register") {
+//                // Implement the registration logic here
+//                // After registration is successful:
+//                self.isRegistered = true // Set this to true to trigger navigation
+//            }
+//            .background(
+//                NavigationLink(destination: NameView(), isActive: $isRegistered) {
+//                    EmptyView()
+//                }
+//                .hidden() // Hide the NavigationLink itself, we only want the functionality
+//            )
+//        }
+//        .navigationTitle("Register")
+//    }
+//}
