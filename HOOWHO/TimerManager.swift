b5371fb9f3d4a255134c28321584a5df25424392
Keith Jung <60eokk@gmail.com>
2024-01-30 20:55:59 -0600

    decided to get rid of timermanagement and implement other functions first

--- HOOWHO/TimerManager.swift
+++ HOOWHO/TimerManager.swift
@@ -1,58 +1,58 @@
+////
+////  TimerManager.swift
 //
-//  TimerManager.swift
-
-
-import Foundation
-import Combine
-
-class TimerManager: ObservableObject {
-    static let shared = TimerManager()
-    @Published var timeRemaining: Int
-
-    private var timer: Timer?
-    private let totalTime: Int = 5  // 30 minutes
-
-    private init() {
-        self.timeRemaining = totalTime
-    }
-
-    func startTimer() {
-        timer?.invalidate() // Invalidate any existing timer
-        self.timeRemaining = totalTime
-
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
-            DispatchQueue.main.async {
-                if let self = self {
-                    if self.timeRemaining > 0 {
-                        self.timeRemaining -= 1
-                    } else {
-                        self.timer?.invalidate()
-                    }
-                }
-            }
-        }
-    }
-    
-    func resetTimer() {
-        timer?.invalidate()
-        self.timeRemaining = totalTime
-    }
-
-
-
-    private func timeTick() {
-        if timeRemaining > 0 {
-            DispatchQueue.main.async {
-                self.timeRemaining -= 1
-            }
-        } else {
-            timer?.invalidate()
-            timer = nil
-        }
-    }
-
-
-    deinit {
-        timer?.invalidate()
-    }
-}
+//
+//import Foundation
+//import Combine
+//
+//class TimerManager: ObservableObject {
+//    static let shared = TimerManager()
+//    @Published var timeRemaining: Int
+//
+//    private var timer: Timer?
+//    private let totalTime: Int = 5  // 30 minutes
+//
+//    private init() {
+//        self.timeRemaining = totalTime
+//    }
+//
+//    func startTimer() {
+//        timer?.invalidate() // Invalidate any existing timer
+//        self.timeRemaining = totalTime
+//
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+//            DispatchQueue.main.async {
+//                if let self = self {
+//                    if self.timeRemaining > 0 {
+//                        self.timeRemaining -= 1
+//                    } else {
+//                        self.timer?.invalidate()
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    func resetTimer() {
+//        timer?.invalidate()
+//        self.timeRemaining = totalTime
+//    }
+//
+//
+//
+//    private func timeTick() {
+//        if timeRemaining > 0 {
+//            DispatchQueue.main.async {
+//                self.timeRemaining -= 1
+//            }
+//        } else {
+//            timer?.invalidate()
+//            timer = nil
+//        }
+//    }
+//
+//
+//    deinit {
+//        timer?.invalidate()
+//    }
+//}
